30df35191b449b0d094b38299638861f669a02f6
tanyamg <tanyamgeo@gmail.com>
2023-01-29 14:13:59 +0200

    Add Project Ref Add first tree tests

--- Collection.UnitTests/CollectionTests.cs
+++ Collection.UnitTests/CollectionTests.cs
@@ -1,12 +1,41 @@
+using Collections;
+
 namespace Collection.UnitTests
 {
     public class CollectionTests
     {
         
         [Test]
-        public void Test1()
+        public void Test_Collection_EmptyConstructor() 
         {
-            Assert.Pass();
+            // Arrange
+            var nums = new Collection<int>();
+
+            // Assert
+            Assert.That(nums.ToString(), Is.EqualTo("[]"));
+
         }
+        [Test]  
+        public void Test_Collection_ConstructorSingleItem()
+        {
+            //Arrange
+            var nums = new Collection<int>(5);
+
+            //Assert
+            Assert.That(nums[0], Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Test_Collection_ConstructorMultipleItems()
+        {
+            //Arrange
+            var nums = new Collection<int>(5, 10, 15);
+
+            //Assert
+            Assert.That(nums.ToString(), Is.EqualTo("[5, 10, 15]"));
+        }
+
+
+
     }
 }
